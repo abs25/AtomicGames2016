d9adfc1b3fa705ecb8870824cb1191c92564f4ee
abs25 <abs25@students.calvin.edu>
2016-10-22 08:21:49 -0400

    test commit

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello");
-  }
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().create();
+        gson.toJson("Hello", System.out);
+        gson.toJson(123, System.out);
+    }
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-# AtomicGames2016
