e0179f3b5e685293c06c3417896f2c385b3acc46
abs25 <abs25@students.calvin.edu>
2016-10-22 11:30:29 -0400

    Get board state

--- Main.java
+++ Main.java
@@ -4,25 +4,33 @@ import java.util.Arrays;
 import org.apache.commons.cli.*;
 
 public class Main {
-    public static void main(String[] args) {
-
+  public static void main(String[] args) {
+    try {
+      //add the potential flags for cmd args
       Options options = new Options();
+      options.addOption("b", true, "get the board");
+      options.addOption("p", true, "what's my name?");
+      options.addOption("t", true, "timer length");
+
       CommandLineParser parser = new DefaultParser();
       CommandLine cmd = parser.parse(options, args);
 
+      //get the arguments
       String boardJson = cmd.getOptionValue("b");
       String playerName = cmd.getOptionValue("p");
       String time = cmd.getOptionValue("t");
 
-      System.out.println(boardJson + "\n" + playerName + "\n" + time );
 
-      // Gson gson = new Gson();
-      //
-      // int[][] board = gson.fromJson(args[0], int[][].class);
-      // for(int i = 0; i < board.length; i++){
-      //   System.out.println(Arrays.toString(board[i]));
-      // }
+      Gson gson = new Gson();
 
-      //Board board = gson.fromJson(json1, Board.class);
+      //put the board in JSON format
+      int[][] board = gson.fromJson(boardJson, int[][].class);
+      for(int i = 0; i < board.length; i++){
+        System.out.println(Arrays.toString(board[i]));
+      }
+
+    }catch(Exception e){
+      System.out.println("it broke :(");
     }
+  }
 }
