1fac9f2131ee0d4ac862f74d77f79c5ef88f77e2
Kirby <codemonkey941@gmail.com>
2016-10-22 10:57:38 -0400

    Add changes to shell script and Main.

--- Main.java
+++ Main.java
@@ -1,17 +1,27 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.util.Arrays;
-
+import org.apache.commons.cli.*;
 
 public class Main {
     public static void main(String[] args) {
 
-      Gson gson = new Gson();
+      Options options = new Options();
+      CommandLineParser parser = new DefaultParser();
+      CommandLine cmd = parser.parse(options, args);
 
-      int[][] board = gson.fromJson(args[0], int[][].class);
-      for(int i = 0; i < board.length; i++){
-      System.out.println(Arrays.toString(board[i]));
-      }
+      String boardJson = cmd.getOptionValue("b");
+      String playerName = cmd.getOptionValue("p");
+      String time = cmd.getOptionValue("t");
+
+      System.out.println(boardJson + "\n" + playerName + "\n" + time );
+
+      // Gson gson = new Gson();
+      //
+      // int[][] board = gson.fromJson(args[0], int[][].class);
+      // for(int i = 0; i < board.length; i++){
+      //   System.out.println(Arrays.toString(board[i]));
+      // }
 
       //Board board = gson.fromJson(json1, Board.class);
     }
